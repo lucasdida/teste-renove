e1203e2167dd9120f7a17f3bfe88cc85a407a5b3
lucasdida <lucasdida@hotmail.com>
2020-09-22 18:38:19 -0300

    footer finalizado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,12 +31,12 @@
                 <form class="form-inline form-search">
                     <input class="form-control mr-sm-2 search-box" style="width:100%" type="search" placeholder="Busque aqui o seu produto" aria-label="Search">
                     <button class="submit-search" type="submit">
-                        <ion-icon name="search-outline" style="font-size: 1.8rem;"></ion-icon>
+                        <i class="fas fa-search" style="font-size: 1.5rem; color: gray"></i>
                     </button>
                 </form>
             </div>
             <div class="col-sm-2">
-                Olá, <span class="font-weight-bold" style="font-size: 1.2rem;">Carlos.</span><ion-icon size="large" name="cart-outline" style="vertical-align:middle;"></ion-icon>
+                Olá, <span class="font-weight-bold" style="font-size: 1.2rem;">Carlos.</span><ion-icon name="cart-outline" size="large" style="vertical-align:middle"></ion-icon>
             </div>
         </nav>
         <nav class="navbar navbar-light navbar-bg-light">
@@ -64,53 +64,46 @@
 
 
         <!-- FOOTER -->
-        <footer class="page-footer pt-4 footer-bg-dark">
-            <div class="container text-md-left footer-dark">
+        <footer class="page-footer pt-4 pb-4 footer-bg-dark">
+            <div class="container-fluid text-md-left footer-dark">
                 <div class="row">
-                    <div class="footer-text-dark" style="margin-right:-40px;">
-                        <ul class="list-unstyled">
-                            <li><ion-icon style="font-size:60px;" name="car"></ion-icon></li>
-                        </ul>
+                    <div class="col-sm-3 d-flex">
+                        <div class="footer-text-dark" style="margin-right:1rem;">
+                        <i class="fas fa-truck" style="font-size: 50px;"></i>
+                        </div>
+                        <div class="footer-text-dark">
+                            <span class="font-weight-bold" style="font-size:1.2rem;">Frete Grátis</span>
+                            <span class="d-block">Acima de R$ 200,00</span>
+                        </div>
                     </div>
-                    <div class="col-md-2 mx-auto footer-text-dark">
-                        <ul class="list-unstyled">
-                            <li><h5>Frete Grátis</h5></li>
-                            <li>Acima de R$ 200,00</li>
-                        </ul>
+                    <div class="col-sm-3 d-flex">
+                        <div class="footer-text-dark" style="margin-right:1rem;">
+                            <i class="fas fa-credit-card" style="font-size: 50px;"></i>
+                        </div>
+                        <div class="footer-text-dark">
+                            <span class="font-weight-bold" style="font-size:1.2rem;">Em até 10x sem juros</span>
+                            <span class="d-block">No cartão de crédito</span>
+                        </div>
                     </div>
-                    <div class="footer-text-dark" style="margin-right:-40px;">
-                        <ul class="list-unstyled">
-                            <li><ion-icon style="font-size:60px;" name="card"></ion-icon></li>
-                        </ul>
+                    <div class="col-sm-3 d-flex">
+                        <div class="footer-text-dark" style="margin-right:1rem;">
+                            <i class="fas fa-truck" style="font-size: 50px;"></i>
+                        </div>
+                        <div class="footer-text-dark">
+                            <span class="font-weight-bold" style="font-size:1.2rem;">Frete Grátis</span>
+                            <span class="d-block">Acima de R$ 200,00</span>
+                        </div>
                     </div>
-                    <div class="col-md-3 mx-auto footer-text-dark">
-                        <ul class="list-unstyled">
-                            <li><h5>Em até 10x sem juros</h5></li>
-                            <li>No cartão de crédito</li>
-                        </ul>
+                    <div class="col-sm-3 d-flex">
+                        <div class="footer-text-dark" style="margin-right:1rem;">
+                            <i class="fas fa-credit-card" style="font-size: 50px;"></i>
+                        </div>
+                        <div class="footer-text-dark">
+                            <span class="font-weight-bold" style="font-size:1.2rem;">Em até 10x sem juros</span>
+                            <span class="d-block">No cartão de crédito</span>
+                        </div>
                     </div>
-                    <div class="footer-text-dark" style="margin-right:-40px;">
-                        <ul class="list-unstyled">
-                            <li><ion-icon style="font-size:60px;" name="car"></ion-icon></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-2 mx-auto footer-text-dark">
-                        <ul class="list-unstyled">
-                            <li><h5>Frete Grátis</h5></li>
-                            <li>Acima de R$ 200,00</li>
-                        </ul>
-                    </div>
-                    <div class="footer-text-dark" style="margin-right:-40px;">
-                        <ul class="list-unstyled">
-                            <li><ion-icon style="font-size:60px;" name="card"></ion-icon></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3 mx-auto footer-text-dark">
-                        <ul class="list-unstyled">
-                            <li><h5>Em até 10x sem juros</h5></li>
-                            <li>No cartão de crédito</li>
-                        </ul>       
-                    </div>
+
                 </div>
             </div>
         </footer>
@@ -120,7 +113,7 @@
                     <img src="/images/bem_menininha_logo.png" width="150" height="38" class="d-inline-block align-top center" alt="">
                 </div>
             </div>
-            <hr>
+            <hr class="horizontal-line">
             <div class="container text-center text-md-left">
                 <div class="row">
                     <div class="col-md-2 mx-auto footer-text">
@@ -158,6 +151,7 @@
                             </li>
                         </ul>
                     </div>
+                    <div class="vertical-line"></div>
                     <hr class="clearfix w-100 d-md-none">
                     <div class="col-md-2 mx-auto footer-text">
                         <h5 class="font-weight-bold mt-3 mb-4">Institucional</h5>
@@ -176,6 +170,7 @@
                             </li>
                         </ul>
                     </div>
+                    <div class="vertical-line"></div>
                     <hr class="clearfix w-100 d-md-none">
                     <div class="col-md-2 mx-auto footer-text">
                         <h5 class="font-weight-bold mt-3 mb-4">Redes Sociais</h5>
@@ -188,6 +183,7 @@
                             </li>            
                         </ul>
                     </div>
+                    <div class="vertical-line"></div>
                     <hr class="clearfix w-100 d-md-none">
                     <div class="col-md-2 mx-auto footer-text">
                         <h5 class="font-weight-bold mt-3 mb-4">Central de Ajuda</h5>
@@ -211,7 +207,7 @@
                     </div>
                 </div>
             </div>
-            <hr>
+            <hr class="horizontal-line">
             <ul class="list-unstyled list-inline text-center py-2 footer-text" style="margin-top:-15px;">
                 <li class="list-inline-item">
                     © 2020 Bemmenininha • Todos os direitos reservados. Avenida Presidente Kennedy, 3106, Aviação Praia Grande, São Paulo, Br
@@ -221,6 +217,7 @@
 
         <script src="{{ asset('js/jquery.js') }}"></script>
         <script src="{{ asset('js/bootstrap.js') }}"></script>
+        <script src="https://kit.fontawesome.com/3c18d26987.js" crossorigin="anonymous"></script>
         <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
     </body>
 </html>
